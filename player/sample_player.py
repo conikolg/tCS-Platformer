de90d3825a5e724ea0b663e3d087a64a08052ad9
Jared Ellis <2008ellis@gmail.com>
2022-07-05 14:25:22 -0500

    Added character sprite animations for idle and run. Added sample player class with functions to handle animation, movement, rendering. Connected sample player class with sample scene. Refactored sample scene render and update functions.

---
+++ player/sample_player.py
@@ -0,0 +1,73 @@
+import pygame
+import os
+
+
+class Player(pygame.sprite.Sprite):
+    """
+    Class for initializing a player and controller.
+    """
+
+    def __init__(self, char_type, x, y, scale, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.alive = True
+        self.char_type = char_type
+        self.speed = speed
+        self.direction = 1
+        self.flip = False
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
+
+        # Player movement
+        self.moving_left = False
+        self.moving_right = False
+
+        animation_types = ["idle", "run", "jump"]
+        for animation in animation_types:
+            temp_list = []
+            num_of_frames = len(os.listdir(f'{self.char_type}/animations/{animation}'))
+            for i in range(num_of_frames):
+                img = pygame.image.load(f'{self.char_type}/animations/{animation}/{i}.png')
+                img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+                temp_list.append(img)
+            self.animation_list.append(temp_list)
+
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def move(self, moving_left, moving_right):
+        dx = 0
+        dy = 0
+
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def update_animation(self):
+        ANIMATION_COOLDOWN = 100
+        self.image = self.animation_list[self.action][self.frame_index]
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+    def update_action(self, new_action):
+        if new_action != self.action:
+            self.action = new_action
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
+
+    def draw(self, screen):
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
--- scenes/sample_scene.py
+++ scenes/sample_scene.py
@@ -1,6 +1,7 @@
 import pygame
 
 from scenes.base_scene import BaseScene
+from player.sample_player import Player
 
 
 class SampleScene(BaseScene):
@@ -14,6 +15,8 @@ class SampleScene(BaseScene):
         # Perform any one-off operations here
         self.reset()
 
+        self.player = Player("player", 100, 100, 1, 5)
+
     def reset(self):
         """
         Puts the player in the starting location.
@@ -21,7 +24,6 @@ class SampleScene(BaseScene):
         Custom function specific to this scene.
         :return: None
         """
-
         self.player_hitbox = pygame.rect.Rect(50, 50, 50, 50)
 
     def handle_events(self, events: list[pygame.event.Event]):
@@ -42,19 +44,27 @@ class SampleScene(BaseScene):
         # Get current keyboard state
         keys = pygame.key.get_pressed()
 
+        # Set player movement boolean to key conditional
+        self.player.moving_right = keys[pygame.K_RIGHT] or keys[pygame.K_d]
+        self.player.moving_left = keys[pygame.K_LEFT] or keys[pygame.K_a]
+
         # Move player (standard coordinate plane)
-        dx, dy = 0, 0
-        if keys[pygame.K_UP] or keys[pygame.K_w]:
-            dy += self.player_directional_speed
-        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            dy -= self.player_directional_speed
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            dx -= self.player_directional_speed
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            dx += self.player_directional_speed
+        # dx, dy = 0, 0
+        # if keys[pygame.K_UP] or keys[pygame.K_w]:
+        #     # dy += self.player_directional_speed
+        #     pass
+        # if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        #     # dy -= self.player_directional_speed
+        #     pass
+        # if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        #     # dx -= self.player_directional_speed
+        #     self.player.moving_left = True
+        # if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        #     # dx += self.player_directional_speed
+        #     self.player.moving_right = True
 
         # Apply standard coordinate plane deltas to pygame coordinate plane
-        self.player_hitbox.move_ip(dx, -dy)
+        # self.player_hitbox.move_ip(dx, -dy)
 
     def render(self, screen: pygame.Surface):
         """
@@ -64,16 +74,27 @@ class SampleScene(BaseScene):
         # White background
         screen.fill((255, 255, 255))
 
-        # Draw player as a box with a few colors
-        pygame.draw.rect(
-            surface=screen, color=(59, 137, 255),  # Blue top-left quarter box
-            rect=(self.player_hitbox.left, self.player_hitbox.top,
-                  self.player_hitbox.width / 2, self.player_hitbox.height / 2))
-        pygame.draw.rect(
-            surface=screen, color=(33, 255, 118),  # Green top-right quarter box
-            rect=(self.player_hitbox.centerx, self.player_hitbox.top,
-                  self.player_hitbox.width / 2, self.player_hitbox.height / 2))
-        pygame.draw.rect(
-            surface=screen, color=(240, 118, 48),  # Orange bottom half box
-            rect=(self.player_hitbox.left, self.player_hitbox.centery,
-                  self.player_hitbox.width, self.player_hitbox.height / 2))
+        # Draw player and update sprite animation
+        self.player.update_animation()
+        self.player.draw(screen)
+
+        if self.player.alive:
+            if self.player.moving_left or self.player.moving_right:
+                self.player.update_action(1)  # 1: run
+            else:
+                self.player.update_action(0)  # 0: idle
+            self.player.move(self.player.moving_left, self.player.moving_right)
+
+        # # Draw player as a box with a few colors
+        # pygame.draw.rect(
+        #     surface=screen, color=(59, 137, 255),  # Blue top-left quarter box
+        #     rect=(self.player_hitbox.left, self.player_hitbox.top,
+        #           self.player_hitbox.width / 2, self.player_hitbox.height / 2))
+        # pygame.draw.rect(
+        #     surface=screen, color=(33, 255, 118),  # Green top-right quarter box
+        #     rect=(self.player_hitbox.centerx, self.player_hitbox.top,
+        #           self.player_hitbox.width / 2, self.player_hitbox.height / 2))
+        # pygame.draw.rect(
+        #     surface=screen, color=(240, 118, 48),  # Orange bottom half box
+        #     rect=(self.player_hitbox.left, self.player_hitbox.centery,
+        #           self.player_hitbox.width, self.player_hitbox.height / 2))
