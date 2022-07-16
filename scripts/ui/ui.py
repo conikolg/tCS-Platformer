c53358ff9d68479c1af1b9e841be09914de84471
Jared Ellis <2008ellis@gmail.com>
2022-07-15 22:39:45 -0500

    Added UI elements for key instructions and remapped player input system.

--- scripts/ui/ui.py
+++ scripts/ui/ui.py
@@ -9,34 +9,45 @@ class UI:
         self.x = 25
         self.y = 25
         self.player = player
-        print(list(self.keys.items())[0])
 
     @staticmethod
     def load_keys(size: tuple) -> dict[pygame.Surface, str]:
         # Create container for keys
         move_keys: dict[pygame.Surface, str] = dict()
         # Start at content root for this key type
-        root_movekeys_dir = sorted(Path(f"assets/keys/movement").iterdir())
+        root_movekeys_dir = sorted(Path(f"assets/keys").iterdir())
 
-        for key in root_movekeys_dir:
-            # Load the key
-            img: pygame.Surface = pygame.image.load(key).convert_alpha()
-            # Scale it
-            img = pygame.transform.scale(img, size)
-            # Add it to big key dictionary
-            move_keys[img] = "Desc of key"
+        for key_type in root_movekeys_dir:
+            for key in key_type.iterdir():
+                # Load the key
+                img: pygame.Surface = pygame.image.load(key).convert_alpha()
+                # Scale it
+                img = pygame.transform.scale(img, size)
+                # Add it to big key dictionary
+                # How do I do this better than this
+                # Manually create a dictionary with surface image and desc instead?
+                if "A.png" in str(key):
+                    move_keys[img] = "Move Left"
+                elif "D.png" in str(key):
+                    move_keys[img] = "Move Right"
+                elif "W.png" in str(key):
+                    move_keys[img] = "Jump"
+                elif "J.png" in str(key):
+                    move_keys[img] = "Shoot"
+                else:
+                    move_keys[img] = "Desc of key"
 
         return move_keys
 
     def draw(self, screen):
         # No way in hell this is the best way to do it
         # But it works so fuck you
-        i = 0
+        i = 1
         for image, desc in self.keys.items():
             screen.blit(image, (self.x, i*self.y))
             font = pygame.font.Font("assets/dogicapixelbold.ttf", 12)
             text = font.render(desc, True, (255, 255, 255))
             screen.blit(text, dest=(
-                self.x - text.get_width() / 2 + 100,
+                self.x + 50,
                 i*self.y + text.get_height() / 2))
             i += 1
